e68f3f1576b2d141fc13c56e5715079713c648a0
jamesghazard <james.g.hazard@gmail.com>
2018-04-04 18:15:42 +0200

    point the GitHub references to the right repo.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 DEFINE('ROOT' ,dirname(__FILE__) ); // Root directory of site
 
-DEFINE('URLFORREPO', 'https://github.com/CommonAccord/Cmacc-Org');	// The repo home
+DEFINE('URLFORREPO', 'https://github.com/CommonAccord/GISP_SemanticWeb_Cmacc');	// The repo home
 
 DEFINE('LANDING_MD', 'S/About/Landing2.md');	// The website home moved because some bot keeps overwriting Landing.md
 
